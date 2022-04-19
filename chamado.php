976ec6a8007e220d4a9a60d68132977952c33e7a
TheusGley <81973986+TheusGley@users.noreply.github.com>
2022-04-19 17:55:26 -0400

    Add files via upload
    Site chamados

---
+++ chamado.php
@@ -0,0 +1,67 @@
+<html>
+        <head>
+      
+      <title>Chamados Cti </title>
+
+      <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+  
+      <label id="titulo"> Chamados Cti </label><br><br>
+
+</head>
+<body>
+        
+
+<form method="post" ac id="Form">
+
+<label  id="label1">   Coloque seu email:   </label><br><br>
+<input type="text"   size="50" id="estilo1" placeholder=" defensoria@dp.mt.gov.br"  >
+<br><br>
+
+<label  id="label1">   Coloque seu Nome Completo:   </label><br><br>
+
+<input type="text"   size="50" id="estilo1" placeholder=""  >
+<br><br>
+<label  id="label1">   Coloque seu Núcleo:   </label><br><br>
+
+
+<input type="text"   size="50" id="estilo1" placeholder=""  >
+<br><br>
+<label  id="label1">   Coloque seu Setor:   </label><br><br>
+
+
+<input type="text"   size="50" id="estilo1" placeholder=""  >
+<br><br>
+<label  id="label1">   Coloque seu Telefone:   </label><br><br>
+
+
+<input type="text"   size="50" id="estilo1" placeholder=""  >
+<br><br>
+<label  id="label1">   Coloque seu Descreva o Problema:   </label><br><br>
+
+
+<input type="text"   size="50" id="estilo1" placeholder=""  >
+<br><br>
+<label  id="label1">   Informações adicionais:   </label><br><br>
+
+
+
+
+
+
+
+<textarea> </textarea> <br><br>
+
+
+
+<input type="submit" name="sub" value="Cadastrar!" id="btn">
+
+
+</form>
+
+</body>
+
+
+</html>
+        
+
+   
