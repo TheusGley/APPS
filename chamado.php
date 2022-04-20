2cf6145b737febc79d98541146fb858efbc743d9
TheusGley <81973986+TheusGley@users.noreply.github.com>
2022-04-20 17:38:33 -0400

    Add files via upload

--- chamado.php
+++ chamado.php
@@ -1,59 +1,52 @@
 <html>
-        <head>
+  <head>
+  
+  
+ <?php   include ( " conexaodb.php "); ?>
       
       <title>Chamados Cti </title>
 
-      <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+     <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
   
-      <label id="titulo"> Chamados Cti </label><br><br>
+     <label id="titulo"> Chamados Cti </label><br><br>
 
-</head>
+  </head>
 <body>
         
 
-<form method="post" ac id="Form">
+<form method="post" action=" " id="Form">
 
-<label  id="label1">   Coloque seu email:   </label><br><br>
-<input type="text"   size="50" id="estilo1" placeholder=" defensoria@dp.mt.gov.br"  >
+<label  id="label1">   Coloque seu email:   </label><br><input type="text"   size="50" id="estilo1" placeholder=" defensoria@dp.mt.gov.br"  >
 <br><br>
 
-<label  id="label1">   Coloque seu Nome Completo:   </label><br><br>
-
-<input type="text"   size="50" id="estilo1" placeholder=""  >
+<label  id="label1">   Coloque seu Nome Completo:   </label><input type="text"   size="50" id="estilo1" placeholder=""  >
 <br><br>
-<label  id="label1">   Coloque seu Núcleo:   </label><br><br>
 
-
-<input type="text"   size="50" id="estilo1" placeholder=""  >
+<label  id="label1">   Coloque seu Núcleo:   </label><input type="text"   size="50" id="estilo1" placeholder=""  >
 <br><br>
-<label  id="label1">   Coloque seu Setor:   </label><br><br>
 
-
-<input type="text"   size="50" id="estilo1" placeholder=""  >
+<label  id="label1">   Coloque seu Setor:   </label><input type="text"   size="50" id="estilo1" placeholder=""  >
 <br><br>
-<label  id="label1">   Coloque seu Telefone:   </label><br><br>
 
 
-<input type="text"   size="50" id="estilo1" placeholder=""  >
+
+<label  id="label1">   Coloque seu Telefone:   </label><input type="text"   size="50" id="estilo1" placeholder=""  >
 <br><br>
-<label  id="label1">   Coloque seu Descreva o Problema:   </label><br><br>
 
 
-<input type="text"   size="50" id="estilo1" placeholder=""  >
+
+<label  id="label1">   Coloque seu Descreva o Problema:   </label><input type="text"   size="50" id="estilo1" placeholder=""  >
 <br><br>
-<label  id="label1">   Informações adicionais:   </label><br><br>
 
 
 
 
-
-
-
-<textarea> </textarea> <br><br>
+<label  id="label1">   Informações adicionais:   </label><textarea> </textarea> <br><br>
 
 
 
 <input type="submit" name="sub" value="Cadastrar!" id="btn">
+
 
 
 </form>
